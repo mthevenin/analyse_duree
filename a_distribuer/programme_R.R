f96a925c788534bb4e87c228c5d97990788f1538
mthevenin <33457234+mthevenin@users.noreply.github.com>
2022-06-13 12:39:44 +0200

    Add files via upload

--- a_distribuer/programme_R.R
+++ a_distribuer/programme_R.R
@@ -1,3 +1,6 @@
+# maj juin 2022
+# version antérieure de cox.zph (=>cox.zphold) préférable pour durées discrètes/groupées
+
 #install.packages("survival")
 #install.packages("survminer")
 #install.packages("flexsurv")
@@ -12,6 +15,7 @@
 #install.packages("stringr")
 #install.packages("gtsummary")
 #install.packages("nnet")
+#install.packages("muhaz")
 library(survival)
 library(survminer)
 library(flexsurv)
@@ -25,19 +29,20 @@ library(tibble)
 library(stringr)
 library(gtsummary)
 library(nnet)
+library(muhaz)
 
-# installation survival v2.44-1 (30 mars 2019) => problème test Grambsch-Therneau
+# installation survival v2.44-1 (30 mars 2019) => problÃÂ¨me test Grambsch-Therneau
 #require(remotes)
 #install_version("survival", version = "2.44-1", repos = "http://cran.us.r-project.org")
 
-# chargement base
+# Chargement base
 library(readr)
 trans <- read_csv("https://raw.githubusercontent.com/mthevenin/analyse_duree/master/bases/transplantation.csv")
 
-# non paramétrique
+# Non parametrique
 
 ## km
-fit <- survfit(Surv(time, status) ~ x, data = base)
+# syntaxe: fit <- survfit(Surv(time, status) ~ x, data = df)
 
 fit <- survfit(Surv(stime, died) ~ 1, data = trans)
 fit
@@ -46,6 +51,12 @@ plot(fit)
 
 ggsurvplot(fit, conf.int = TRUE)
 ggsurvplot(fit, conf.int = TRUE, risk.table = TRUE)
+
+# estimation et visualisation de la fonction de risque
+# technique de lissage avec de nombreux paramÃ¨tre. Ici paramÃ¨tres par dÃ©faut.
+
+haz = muhaz(trans$stime,trans$died)
+plot(haz)
 
 ### comparaison
 fit <- survfit(Surv(stime, died) ~ surgery, data = trans)
@@ -66,38 +77,46 @@ plot(a)
 # risques proportionnels
 
 # Cox
-
 coxfit = coxph(formula = Surv(stime, died) ~ year + age + surgery, data = trans)
 summary(coxfit)
 ggforest(coxfit)
 
 ## test ph attention diff v2 versus v3
+### test GB (resultat different cours si v3)
 
-### test GB (résultat différent cours si v3)
+### avec Récupération de l'ancienne version du test: fonction cox.zphold
+## lien fichier: https://github.com/mthevenin/analyse_duree/tree/main/cox.zphold
+source("D:/D/Marc/SMS/FORMATIONS/2022/Durée2/a distribuer/cox.zphold.R")
+cox.zphold(coxfit)
+cox.zphold(coxfit, transform="identity")
 
-cox.zph(coxfit)
-cox.zph(coxfit, transform="identity")
-
-### méthode ols: corrélation residus/t
+### methode ols: correlation residus/t
+### certains conseille d'estimer un gls (generalized least square => correlation des erreurs de la regression dans le temps)
 
 resid= resid(coxfit, type="scaledsch")
+
+coln = c(varnames)
+colnames(resid) = c(coln)
+
+times    = as.numeric(dimnames(resid)[[1]])
+varnames = names(fit$coefficients)
+
 resid = data.frame(resid)
+resid = cbind(resid, t=times)
 
-resid= rownames_to_column(resid, var = "Z")
+year    = summary(lm(year~t, data=resid))
+age     = summary(lm(age~t, data=resid))
+surgery = summary(lm(surgery~t, data=resid))
 
-resid$t = str_sub(resid$Z, 2, )
-resid$t=as.numeric(resid$t)
-resid$t=round(resid$t, digits=0)
-
-year    = summary(lm(X1~t, data=resid))
-age     = summary(lm(X2~t, data=resid))
-surgery = summary(lm(X3~t, data=resid))
+##########
+#p-values#
+##########
 
 year$coefficients[,4] 
 age$coefficients[,4] 
 surgery$coefficients[,4] 
 
-## intéraction
+## interaction
 
 coxfit2 = coxph(formula = Surv(stime, died) ~ year + age + surgery + tt(surgery), data = trans, tt = function(x, t, ...) x*t)
 summary(coxfit2)
@@ -113,9 +132,10 @@ summary(tvcfit)
 
 ggforest(tvcfit, data=trans)
 
+
 # logistique temps discret
 
-## durée continue
+## duree continue
 library("tidyr")
 
 ###
@@ -135,9 +155,9 @@ dt$t2=dt$t^2
 dtfit = glm(died ~ t + t2 + year + age + surgery, data=dt, family="binomial")
 summ(dtfit)
 
-## durée groupée/discrète (le début vise pour l'exemple à regrouper les durées)
+## duree groupee/discrete (le debut vise pour l'exemple a regrouper les durees)
 
-### groupement des durées pour l'exemple
+### groupement des durees pour l'exemple
 dt$ct4 <- quantcut(dt$t)
 table(dt$ct4) 
 dt$n = ave(dt$x,dt$id, dt$ct4, FUN=cumsum)
@@ -147,9 +167,9 @@ dt2 = subset(dt, n==N)
 fit = glm(died ~ ct4 + year + age + surgery, data=dt2, family=binomial)
 summ(fit)
 
-# Compléments
+# Complements
 
-## modèles paramétriques standard (seulement weibull)
+## modeles parametriques standard (seulement weibull)
 
 ### aft
 weibull = survreg(formula = Surv(stime, died) ~ year + age + surgery, data = trans, dist="weibull")
@@ -181,7 +201,7 @@ ggcompetingrisks(fit = ic)
 ggcompetingrisks(fit = ic, multiple_panels = F)
 
 ## logit multinomial
-## fonction multinom de nnet: l'output de l'angoisse (à quand une maj?????)
+## fonction multinom de nnet: l'output de l'angoisse (a quand une maj?????)
 ## utiliser gtsummary
 
 ### mise en forme
@@ -192,7 +212,7 @@ td$t = ave(td$x, td$id, FUN=cumsum)
 td$t2 = td$t*td$t
 td$e = ifelse(td$t<td$T,0, td$compet)
 
-### modèle
+### modele
 competfit = multinom(formula = e ~ t + t2 + year + age + surgery, data = td)
 summary(competfit)
 z = summary(competfit)$coefficients/summary(competfit)$standard.errors
